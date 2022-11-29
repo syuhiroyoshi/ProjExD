073e4dbea2c1c5f8ecbe8e990606b5a94da2a262
syuhiroyoshi <c0b20076fa@edu.teu.ac.jp>
2022-11-29 15:10:21 +0900

    練習１，２，３，４，５，６，７

--- ex02/calb.py
+++ ex02/calb.py
@@ -1,28 +1,33 @@
+
 import tkinter as tk
 import tkinter.messagebox as tkm
 
+
+# 練習３
 def button_click(event):
     btn = event.widget
     num = btn["text"]
     if num == "=":
-        siki = entry.get()
-        res = eval(siki)
-        entry.delete(0, tk.END)
-        entry.insert(tk.END, res)
-    else:
+        siki = entry.get() # 数式の文字列
+        res = eval(siki) # 数式文字列の評価
+        entry.delete(0, tk.END) # 表示文字列の削除
+        entry.insert(tk.END, res) # 結果の挿入
+    else: # 「=」以外のボタン字
+        #tkm.showinfo("", f"{num}ボタンがクリックされました")
+        # 練習６
         entry.insert(tk.END, num)
 
-    #tkm.showinfo("",f"{num}ボタンがクリックされました")
-
-
+    
+# 練習１
 root = tk.Tk()
 root.geometry("300x500")
 
+# 練習４
 entry = tk.Entry(root, justify="right", width=10, font=("",40))
 entry.grid(row=0, column=0, columnspan=3)
 
-
-r,c =0, 0
+# 練習２
+r, c = 1, 0
 for num in range(9, -1, -1):
     button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30))
     button.grid(row=r, column=c)
@@ -32,10 +37,12 @@ for num in range(9, -1, -1):
         r += 1
         c = 0
 
+# 練習５
 operators = ["+", "="]
 for ope in operators:
-    button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30))
+    button = tk.Button(root, text=f"{ope}", width=4, height=2, font=("", 30))
     button.grid(row=r, column=c)
+    button.bind("<1>", button_click)
     c += 1
     if c%3 == 0:
         r += 1
