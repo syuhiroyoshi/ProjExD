dc819665dab207795a9d5b9b88f14cf2b4949db0
syuhiroyoshi <c0b20076fa@edu.teu.ac.jp>
2022-11-29 14:58:03 +0900

    練習１，２，３，４，５，６，７

---
+++ ex02/calb.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+def button_click(event):
+    btn = event.widget
+    num = btn["text"]
+    if num == "=":
+        siki = entry.get()
+        res = eval(siki)
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, res)
+    else:
+        entry.insert(tk.END, num)
+
+    #tkm.showinfo("",f"{num}ボタンがクリックされました")
+
+
+root = tk.Tk()
+root.geometry("300x500")
+
+entry = tk.Entry(root, justify="right", width=10, font=("",40))
+entry.grid(row=0, column=0, columnspan=3)
+
+
+r,c =0, 0
+for num in range(9, -1, -1):
+    button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30))
+    button.grid(row=r, column=c)
+    button.bind("<1>", button_click)
+    c += 1
+    if c%3 == 0:
+        r += 1
+        c = 0
+
+operators = ["+", "="]
+for ope in operators:
+    button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30))
+    button.grid(row=r, column=c)
+    c += 1
+    if c%3 == 0:
+        r += 1
+        c = 0
+
+root.mainloop()
