a31066604b65056a61d7aac2b6280c4a2107b8ab
syuhiroyoshi <c0b20076fa@edu.teu.ac.jp>
2022-11-29 17:21:14 +0900

    更新練習

--- ex02/calc.py
+++ ex02/calc.py
@@ -2,29 +2,61 @@ import tkinter as tk
 import tkinter.messagebox as tkm
 
 def button_click(event):
-    btn = event.widget
-    txt = btn["text"]
-    tkm.showinfo(txt,f"[{txt}]ボタンが押されました")
+    btn=event.widget
+    txt=btn["text"]
+    entry.insert(tk.END,txt)
 
-root = tk.Tk()
-root.title("おためしか")
-root.geometry("300x500")
+def click_eqall(event):#=の実施
+    eqn=entry.get()
+    res=eval(eqn)
+    entry.delete(0,tk.END)
+    entry.insert(tk.END,str(res))
 
-label = tk.Label(root,
-                text="らべるを書いてみた件",
-                font=("", 20)
-                )
-label.pack()
+def click_clear(event):#1字削除する.
+    cl=entry.get()
+    cll=cl[:-1]
+    entry.delete(0,tk.END)
+    entry.insert(tk.END,str(cll))
 
-button = tk.Button(root, text="押すな")
-button.bind("<1>", button_click)
-button.pack()
+def click_alclear(event):#すべて削除
+    entry.delete(0,tk.END)
+    entry.insert(tk.END)
 
 
-entry = tk.Entry(width=30)
-entry.insert(tk.END, "fugapiyo")
-entry.pack()
 
-#tkm.showerror("title","message")
+if __name__ == '__main__':
+    bt = tk.Tk()
+    bt.geometry("300x420")
 
-root.mainloop()
+    entry = tk.Entry(bt,justify="right",width=10,font=("Times New Roman",40))
+    entry.grid(row=0,column=0,columnspan=4)
+
+    r,c=1,1
+    for i,num in enumerate(["/","*","-","+",9,8,7,6,5,4,3,2,1,0],1):
+        btn =tk.Button(bt,text=num,font=("Times New Roman",15),width=3,command=button_click)
+        btn.bind("<1>",button_click)
+        btn.grid(row=r,column=c,padx=10,pady=10)
+        if i%3==0:
+            r+=1
+            c=0
+        c+=1
+
+
+    btn=tk.Button(bt,text="AC",font=("Times New Roman",15))
+    btn.bind("<1>",click_alclear)
+    btn.grid(row=6,column=1,padx=10,pady=10)
+
+    btn=tk.Button(bt,text="C",font=("Times New Roman",15),width=3)
+    btn.bind("<1>",click_clear)
+    btn.grid(row=6,column=2,padx=10,pady=10)
+
+    btn=tk.Button(bt,text="=",font=("Times New Roman",15),width=3)
+    btn.bind("<1>",click_eqall)
+    btn.grid(row=5,column=3,padx=10,pady=10)
+
+    #btn=tk.Button(bt,text="x²",font=("Times New Roman",15),width=3) #二乗したいけど時間がない
+    #btn.bind("<1>",click_clear)
+    #btn.grid(row=6,column=3,padx=10,pady=10)
+
+    bt.mainloop()
+
